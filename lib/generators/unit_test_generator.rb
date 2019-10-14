579edc315a44a3d0813bd3b2ac23c6fdebbcaf57
nicknish09 <nnmarques97@gmail.com>
2019-10-13 22:06:57 -0300

    generator funcionando

---
+++ lib/generators/unit_test_generator.rb
@@ -0,0 +1,24 @@
+class UnitTestGenerator < Rails::Generators::Base
+  def create_unit_tests
+    require 'json'
+    class_name = args[0]
+    file_name = class_name.downcase
+    puts "criando testes unitarios para #{class_name}..."
+    template = %Q{require 'rails_helper'
+
+RSpec.describe #{class_name}, :type => :model do
+  it "is valid with valid attributes" do
+    expect(#{class_name}.new).to be_valid
+  end
+end
+}
+    File.readlines("#{Rails.root}/tmp/#{class_name}.rb").each do |line|
+      json_string = JSON.parse(line.to_json)
+      puts json_string["method"]
+    end
+
+    create_file "spec/models/#{file_name}_spec.rb", template
+
+    puts "Testes criados."
+  end
+end
