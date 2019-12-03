8078b5e36c8d52a4ef889baa5399abc31e832242
nicknish09 <nnmarques97@gmail.com>
2019-12-03 15:07:24 -0300

    atualizado nao criar testes para metodos repetidos

--- lib/generators/unit_test_generator.rb
+++ lib/generators/unit_test_generator.rb
@@ -17,6 +17,8 @@ RSpec.describe #{class_name}, :type => :#{class_type} do
     expect(#{class_name}.new).to be_valid
   end
 }
+    executed_methods = []
+    executed_arguments = {}
     File.readlines("#{Rails.root}/tmp/#{class_name}.rb").each do |line|
       json_string = JSON.parse(line.strip)
       puts "\nReflecting..."
@@ -25,18 +27,30 @@ RSpec.describe #{class_name}, :type => :#{class_type} do
       meth = json_string["method"]
       cs = json_string["current_state"]
       args = json_string["args"]
-      puts "Executando #{klass}::#{meth} com argumentos #{args} no objeto #{cs}"
-      reflection = reflector.reflect(klass, meth, cs, args)
-      puts "Resultado: #{reflection}"
-      print "\n"
+      class_and_method_name = "#{klass}::#{meth}"
+      if executed_methods.include?(class_and_method_name)
+        if executed_arguments[class_and_method_name.to_sym].include?(args)
+          # do nothing, same test
+        else
+          # same test with other arguments
+          executed_arguments[class_and_method_name.to_sym].push(args)
+        end
+      else # other test for other method
+        executed_methods.push(class_and_method_name)
+        executed_arguments[class_and_method_name.to_sym] = []
+        puts "Executando #{klass}::#{meth} com argumentos #{args} no objeto #{cs}"
+        reflection = reflector.reflect(klass, meth, cs, args)
+        puts "Resultado: #{reflection}"
+        print "\n"
 
-      template_aux = %Q{
+        template_aux = %Q{
   it "should return valid string for method #{meth}" do
     #{class_name.downcase} = #{class_name}.new(#{cs})
     expect(#{class_name.downcase}.#{meth}(#{args.join(', ')})).to eq "#{reflection}"
   end
       }
-      template.concat(template_aux)
+        template.concat(template_aux)
+      end
     end
 
     template.concat(%Q{
