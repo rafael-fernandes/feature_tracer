da2f3a609acf04ac37a4a93b6fc5dff7ac80125a
nicknish09 <nnmarques97@gmail.com>
2019-12-03 15:40:24 -0300

    teste com outros argumentos

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Post, :type => :model do
   it "should return valid string for method truncated_desc" do
     post = Post.new({"id"=>160, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T17:36:05.822Z", "updated_at"=>"2019-12-03T17:36:05.822Z"})
     expect(post.truncated_desc(30)).to eq "Esse é o post 0 post 0 post..."
+    expect(post.truncated_desc(10)).to eq "Esse é ..."
   end
       
   it "should return valid string for method create_date" do
