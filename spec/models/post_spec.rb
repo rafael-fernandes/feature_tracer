59d495f67f3e87a60091a5afc62ec686f22f4dd3
nicknish09 <nnmarques97@gmail.com>
2019-12-03 15:43:10 -0300

    postspec att

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,18 +6,17 @@ RSpec.describe Post, :type => :model do
   end
 
   it "should return valid string for method upcase_title" do
-    post = Post.new({"id"=>160, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T17:36:05.822Z", "updated_at"=>"2019-12-03T17:36:05.822Z"})
+    post = Post.new({"id"=>164, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T18:42:03.412Z", "updated_at"=>"2019-12-03T18:42:03.412Z"})
     expect(post.upcase_title()).to eq "POST 0"
   end
       
   it "should return valid string for method truncated_desc" do
-    post = Post.new({"id"=>160, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T17:36:05.822Z", "updated_at"=>"2019-12-03T17:36:05.822Z"})
+    post = Post.new({"id"=>164, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T18:42:03.412Z", "updated_at"=>"2019-12-03T18:42:03.412Z"})
     expect(post.truncated_desc(30)).to eq "Esse é o post 0 post 0 post..."
-    expect(post.truncated_desc(10)).to eq "Esse é ..."
   end
       
   it "should return valid string for method create_date" do
-    post = Post.new({"id"=>160, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T17:36:05.822Z", "updated_at"=>"2019-12-03T17:36:05.822Z"})
+    post = Post.new({"id"=>164, "title"=>"Post 0", "description"=>"Esse é o post 0 post 0 post 0 post 0 post 0 post 0 post 0", "created_at"=>"2019-12-03T18:42:03.412Z", "updated_at"=>"2019-12-03T18:42:03.412Z"})
     expect(post.create_date()).to eq "03/12/19"
   end
       
