a8324ce74a0fa785e6fa5de97b9ad5746919ada9
nicknish09 <nnmarques97@gmail.com>
2020-04-17 17:46:58 -0300

    criado funcao is phone

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -10,4 +10,9 @@ class String
   def is_float?
     self.to_f.to_s === self
   end
+
+  def is_phone?
+    /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/ === self ||
+        /^\([1-9]{2}\)(?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/ === self
+  end
 end
