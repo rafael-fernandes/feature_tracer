70bb493d8c61d84a723c7432e3c588a847cb7dd3
nicknish09 <nnmarques97@gmail.com>
2020-04-17 14:12:28 -0300

    sessions controller criado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   root to: 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
